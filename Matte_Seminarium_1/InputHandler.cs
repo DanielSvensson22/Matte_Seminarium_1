f6102345065035802a949c513fc11b2be8c5309b
antn <antnl908@gmail.com>
2022-12-05 14:15:14 +0100

    new update

--- Matte_Seminarium_1/InputHandler.cs
+++ Matte_Seminarium_1/InputHandler.cs
@@ -11,29 +11,48 @@ namespace Matte_Seminarium_1
 {
     internal class InputHandler
     {
-        //test
         private Vector2 direction { get; set; }
         private Vector2 mousePosition { get; set; }
         private Vector2 mouseOrigin { get; set; }
         private float distance { get; set;}
 
-        private bool mouseL;
-        private bool space;
+        //private bool mouseL;
+        //private bool space;
 
         private MouseState currentMouse { get; set; }
+        private MouseState previousMouse { get; set; }
         private KeyboardState currentKeyboard { get; set; }
 
 
         public void Update(GameTime gameTime)
         {
+            UpdateStates();
+            UpdateValues();
+        }
+
+        private void UpdateStates()
+        {
             CurrentMouse = Mouse.GetState();
             CurrentKeyboard = Keyboard.GetState();
         }
 
+        private void UpdateValues()
+        {
+            MousePosition = new Vector2(CurrentMouse.X, CurrentMouse.Y);
+            if (CurrentMouseL && !PreviousMouseL) { MouseOrigin = new Vector2(CurrentMouse.X, CurrentMouse.Y); }
+            if (!CurrentMouseL && PreviousMouseL) { Direction = MouseOrigin - MousePosition; Distance = Vector2.Distance(MouseOrigin, MousePosition); }
+        }
+
+        #region States
         public virtual MouseState CurrentMouse
         {
             get { return currentMouse; }
-            set { currentMouse = value; }
+            set { PreviousMouse = currentMouse; currentMouse = value; }
+        }
+        public virtual MouseState PreviousMouse
+        {
+            get { return previousMouse; }
+            set { previousMouse = value; }
         }
         
         public virtual KeyboardState CurrentKeyboard
@@ -41,17 +60,45 @@ namespace Matte_Seminarium_1
             get { return currentKeyboard; }
             set { currentKeyboard = value; }
         }
+        #endregion
 
-        public virtual bool MouseLDown
+        public virtual bool CurrentMouseL
         {
-            //get { if (NewKeyboard.IsKeyDown(Keys.Space)) { space = true; } else { space = false; } return space; }
-            get { if (CurrentMouse.LeftButton == ButtonState.Pressed) { mouseL = true; } else { mouseL = false; } return mouseL; }
+            get { if (CurrentMouse.LeftButton == ButtonState.Pressed) { return true; } else { return false; } }
+        }
+
+        public virtual bool PreviousMouseL
+        {
+            get { if (PreviousMouse.LeftButton == ButtonState.Pressed) { return true; } else { return false; } }
         }
 
         public virtual bool SpaceDown
         {
-            //get { if (NewKeyboard.IsKeyDown(Keys.Space)) { space = true; } else { space = false; } return space; }
-            get { if (CurrentKeyboard.IsKeyDown(Keys.Space)) { space = true; } else { space = false; } return space; }
+            get { if (CurrentKeyboard.IsKeyDown(Keys.Space)) { return true; } else { return false; } }
+        }
+
+        public virtual Vector2 MouseOrigin
+        {
+            get { return mouseOrigin; }
+            set { mouseOrigin = value; }
+        }
+        
+        public virtual Vector2 MousePosition
+        {
+            get { return mousePosition; }
+            set { mousePosition = value; }
+        }
+
+        public virtual Vector2 Direction
+        {
+            get { return direction; }
+            set { direction = value; direction.Normalize(); }
+        }
+        
+        public virtual float Distance
+        {
+            get { return distance; }
+            set { distance = value; }
         }
     }
 }
