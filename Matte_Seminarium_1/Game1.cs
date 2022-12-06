aeb8cd4a3820a89638cdf33f9592e23bc244ff6e
DanielSvensson22 <111700515+DanielSvensson22@users.noreply.github.com>
2022-12-06 23:27:36 +0100

    Added radius alteration and text.

--- Matte_Seminarium_1/Game1.cs
+++ Matte_Seminarium_1/Game1.cs
@@ -10,7 +10,7 @@ namespace Matte_Seminarium_1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        GameState state = GameState.Executing;
+        GameState state = GameState.Preparing;
 
         private Texture2D ballTex;
         private SpriteFont font;
@@ -18,9 +18,16 @@ namespace Matte_Seminarium_1
         Timer timer = new();
         private double hitTime;
         private List<double> hitTimes = new();
+        private List<Vector2> ballAHits = new();
+        private List<Vector2> ballBHits = new();
 
         private Ball ballA;
         private Ball ballB;
+
+        private float radiusA;
+        private float radiusB;
+
+        private InputHandler inputHandler = new();
 
         public Game1()
         {
@@ -45,17 +52,26 @@ namespace Matte_Seminarium_1
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            if(state == GameState.Preparing)
+            font = Content.Load<SpriteFont>("font");
+
+            if (state == GameState.Preparing)
             {
+                timer = new();
+                hitTime = 0;
+
+                radiusA = 20;
+                radiusB = 20;
+
                 hitTimes.Clear();
+                ballAHits.Clear();
+                ballBHits.Clear();
             }
             else if(state == GameState.Executing)
             {
                 ballTex = Content.Load<Texture2D>("ball");
-                font = Content.Load<SpriteFont>("font");
 
-                ballA = new(ballTex, new(100, 100), 20, 1, new(0.05f, 0.1f));
-                ballB = new(ballTex, new(100, 300), 30, 1, new(-0.1f, -0.01f));
+                ballA = new(ballTex, new(100, 100), radiusA, 1, new(0.1f, 0.1f));
+                ballB = new(ballTex, new(300, 300), radiusB, 1, new(-0.1f, -0.1f));
             }
         }
 
@@ -72,7 +88,20 @@ namespace Matte_Seminarium_1
                 LoadContent();
             }
 
-            if (state == GameState.Executing)
+            inputHandler.Update(gameTime);
+
+            if(state == GameState.Preparing)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Enter) && radiusA > 0 && radiusB > 0)
+                {
+                    state = GameState.Executing;
+                    LoadContent();
+                }
+
+                radiusA += inputHandler.RadiusChange(Keys.S, Keys.A);
+                radiusB += inputHandler.RadiusChange(Keys.X, Keys.Z);
+            }
+            else if (state == GameState.Executing)
             {
                 timer.Update(gameTime);
 
@@ -84,11 +113,20 @@ namespace Matte_Seminarium_1
                 {
                     hitTime = timer.time;
                     hitTimes.Add(hitTime);
+
+                    ballAHits.Add(ballA.Origin);
+                    ballBHits.Add(ballB.Origin);
                 }
 
                 //Keeps balls within screen.
                 ballA.WallCollision(Window.ClientBounds.Width, Window.ClientBounds.Height);
                 ballB.WallCollision(Window.ClientBounds.Width, Window.ClientBounds.Height);
+
+                if (Keyboard.GetState().IsKeyDown(Keys.L))
+                {
+                    state = GameState.ListInspection;
+                    LoadContent();
+                }
             }
 
             base.Update(gameTime);
@@ -96,13 +134,19 @@ namespace Matte_Seminarium_1
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Gray);
 
             // TODO: Add your drawing code here
 
             _spriteBatch.Begin();
 
-            if (state == GameState.Executing)
+            if(state == GameState.Preparing)
+            {
+                _spriteBatch.DrawString(font, $"Radius of ball A: {radiusA}", new(400, 200), Color.Red);
+
+                _spriteBatch.DrawString(font, $"Radius of ball B: {radiusB}", new(400, 400), Color.Orange);
+            }
+            else if (state == GameState.Executing)
             {
                 ballA.Draw(_spriteBatch);
                 ballB.Draw(_spriteBatch);
@@ -111,6 +155,23 @@ namespace Matte_Seminarium_1
                 if(hitTimes.Count > 0)
                 {
                     _spriteBatch.DrawString(font, $"Collision at {hitTimes[^1]} seconds.", new(10, Window.ClientBounds.Height - 20), Color.Gold);
+                }
+            }
+            else if(state == GameState.ListInspection)
+            {
+                for(int i = 0; i < hitTimes.Count; i++)
+                {
+                    _spriteBatch.DrawString(font, $"Hit at {hitTimes[i]} seconds.", new(10, 10 + 20 * i), Color.Gold);
+                }
+
+                for (int i = 0; i < ballAHits.Count; i++)
+                {
+                    _spriteBatch.DrawString(font, $"Ball A collided at coordinates: {(int)ballAHits[i].X}; {(int)ballAHits[i].Y}", new(300, 10 + 20 * i), Color.Red);
+                }
+
+                for (int i = 0; i < ballBHits.Count; i++)
+                {
+                    _spriteBatch.DrawString(font, $"Ball B collided at coordinates: {(int)ballBHits[i].X}; {(int)ballBHits[i].Y}", new(600, 10 + 20 * i), Color.Orange);
                 }
             }
 
@@ -123,6 +184,7 @@ namespace Matte_Seminarium_1
         {
             Preparing,
             Executing,
+            ListInspection,
         }
     }
 }
